5dd2b218afdde5afacdb457028863067e26989b0
vestevezbugarin <vestevezbugarin@danielcastelao.org>
2018-10-30 11:22:27 +0100

    Completado

---
+++ Boletin6_3/src/boletin6_3/Boletin6_3.java
@@ -0,0 +1,29 @@
+package boletin6_3;
+
+import javax.swing.JOptionPane;
+
+public class Boletin6_3 {
+
+    /**
+     * Teclear un número e sacar por consola o signo “ + “ se o nº é positivo, o
+     * signo “ –“ se é negativo e “ 0 “ se é cero .
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+
+        int numero = Integer.parseInt(JOptionPane.showInputDialog(null, "Inserte o numero a comprobar: "));
+
+        if (numero == 0) {
+            JOptionPane.showMessageDialog(null, " O número é 0 ");
+        }
+        if (numero > 0) {
+            JOptionPane.showMessageDialog(null, " + ");
+        }
+        if (numero < 0) {
+            JOptionPane.showMessageDialog(null, " - ");
+        }
+
+    }
+
+}
