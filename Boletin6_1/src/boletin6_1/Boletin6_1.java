f7d0740177f422b767a001d1806b58546367b207
vestevezbugarin <vestevezbugarin@danielcastelao.org>
2018-10-30 11:06:49 +0100

    Completado

---
+++ Boletin6_1/src/boletin6_1/Boletin6_1.java
@@ -0,0 +1,18 @@
+package boletin6_1;
+
+import javax.swing.*;
+
+public class Boletin6_1 {
+
+    public static void main(String[] args) {
+
+        int numero = Integer.parseInt(JOptionPane.showInputDialog(null, "Inserte un numero para comprobar se é positivo:"));
+        if (numero < 0) {
+            JOptionPane.showMessageDialog(null, "O número é negativo!", "Negativo", 2);
+        } else {
+            JOptionPane.showMessageDialog(null, "O número é positivo!", "Negativo", 1);
+
+        }
+    }
+
+}
